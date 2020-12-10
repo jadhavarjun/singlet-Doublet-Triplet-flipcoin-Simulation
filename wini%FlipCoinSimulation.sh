72be08d2cdb4f1596f850b1d524fa2d276aee6d0
vinay-ryakala <vinayrakyala@gmail.com>
2020-12-10 11:48:40 +0530

    Adding to show heads or tail UC1

--- wini%FlipCoinSimulation.sh
+++ wini%FlipCoinSimulation.sh
@@ -2,3 +2,14 @@
 
 echo "Welcome to filp coin simulation Wining percentage"
 
+HEADS=1
+TAILS=0
+
+simulationResult=$((RANDOM%2))
+
+if [ $simulationResult -eq $HEADS ]
+then
+	echo "heads is the result"
+else
+	echo "tails is result"
+fi
